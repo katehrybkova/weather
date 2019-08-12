0e63102dc13c8b78591784e417bb50d83813a1c8
katehrybkova <katiushka152701@gmail.com>
2019-08-12 22:58:46 +0300

    Updates

---
+++ precache-manifest.cff498549462c89241fd3d927aeb0e72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "74c2d5070919f7cf3499e8d54a78d0d2",
+    "url": "/weather/static/media/global-warming.74c2d507.svg"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "9c5e3f042c7ebe6a6559",
+    "url": "/weather/static/js/main.5b3e8182.chunk.js"
+  },
+  {
+    "revision": "01e14b44142cb7111fc7",
+    "url": "/weather/static/js/2.1a8fb23f.chunk.js"
+  },
+  {
+    "revision": "9c5e3f042c7ebe6a6559",
+    "url": "/weather/static/css/main.8ae53cdb.chunk.css"
+  },
+  {
+    "revision": "01e14b44142cb7111fc7",
+    "url": "/weather/static/css/2.86a1c46f.chunk.css"
+  },
+  {
+    "revision": "225b67569eadc02856e13ee3b0cbc133",
+    "url": "/weather/index.html"
+  }
+];
