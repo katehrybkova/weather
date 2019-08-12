506a46e2b37a2dac0bd56066b76383c7480cbf6b
katehrybkova <katiushka152701@gmail.com>
2019-08-12 23:00:15 +0300

    working version

--- src/components/Card7Days/Card7Days.js
+++ src/components/Card7Days/Card7Days.js
@@ -3,18 +3,11 @@ import "./Card7Days.css";
 import moment from "moment";
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
-    card: {
-        //   maxWidth: 345,
-    },
     media: {
         height: 140,
     },
@@ -46,8 +39,6 @@ function Card7Days(props) {
                             <Typography component="p">
                                 {el.day.maxtemp_c}°C     
                                  </Typography>
-
-
                         </CardContent>
                     </CardActionArea>
                 </div>
@@ -64,38 +55,3 @@ Card7Days.propTypes = {
 
 export default withStyles(styles)(Card7Days);
 
-
-// let Card7Days = ({ forecastDays }) => 
-
-
-//    ( < div className = "flex7days" >{
-//         forecastDays.map(el => (
-//             <div className="oneCard" key={el.date}>
-//             <h4>{moment(el.date).format('dddd')}</h4>
-//             <h4>{moment(el.date).format('ll')}</h4>
-
-//                 <img src={el.day.condition.icon} alt="weather"/>
-//                 <p>{el.day.condition.text}</p>
-//                 <p>{el.day.maxtemp_c}°C</p>
-
-
-//                 </div>
-//             /* <img width="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpDoKs1Z1zUbwpVpxVU9mpkntkqaUjvVWRYY_EU3cLijVaGQ_o" alt="weather" />
-//              <p>Description</p>
-//              moment().format('LL');
-//              <p>temperature</p>
-//               */
-
-
-//         ))
-//     }</div>)
-
-//     /* < div className = "flex7days" >
-//         <div><h3>Day</h3>
-//             <p>Date</p>
-//             <img width="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpDoKs1Z1zUbwpVpxVU9mpkntkqaUjvVWRYY_EU3cLijVaGQ_o" alt="weather" />
-//             <p>Description</p>
-//             <p>temperature</p></div>
-//     </div ></> */
-
-// export default Card7Days;
